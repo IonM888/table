e2b474cd99d1124ab330ca4bf09bfff133227da2
I-M <i.mustafenco@gmail.com>
2021-03-17 16:01:25 +0200

    s-au important posturile in componenta post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,24 @@ import { StudentService } from './ student.service';
 import { MessageService } from './ message-service.service';
 
 
+export interface Post {
+  title:string,
+  text:string
+  id?:number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
+  posts: Post[]=[
+    {title:'my first Title',text:'Example text component first',id:1},
+    {title:'my second Title',text:'Example text component second',id:2},
+    {title:'my third Title',text:'Example text component third',id:3}
+  ]
 
   students: Student[] = [];
 
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
+import {Post} from '../app.component'
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
+
+  @Input() post: Post
 
   constructor() { }
 
